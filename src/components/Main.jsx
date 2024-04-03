b6b6613c94108e4faf82bf73574e784734b43ae1
mackwick <mackenziewicker@gmail.com>
2024-04-03 08:13:04 -0400

    styled show and main pages, made edit form hideable

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,11 @@
 import Show from "../components/Show";
 import { useLoaderData, Link } from "react-router-dom";
+import creepydoll from "./../images/creepydoll.jpg";
+import drama from "./../images/drama.jpg";
+import comedy from "./../images/comedy.jpg";
+import other from "./../images/other.jpg";
+import reality from "./../images/reality.jpg";
+import scifi from "./../images/scifi.jpg";
 
 function Main(props) {
   const allShows = useLoaderData();
@@ -7,25 +13,42 @@ function Main(props) {
     <div className="genre-container">
       <div className="genre-top">
         <div className="genre-box genre-one">
-          <Link to="/drama/">Drama</Link>
+          <Link className="router-link" to="/drama/">
+            <img src={drama} alt="drama" />
+            <h3>Drama</h3>
+          </Link>
         </div>
         <div className="genre-box genre-two">
-          <Link to="/comedy/">Comedy</Link>
+          <Link className="router-link" to="/comedy/">
+            <img src={comedy} alt="comedy" />
+            <h3>Comedy</h3>
+          </Link>
         </div>
         <div className="genre-box genre-three">
-          <Link to="/scifi/">Science Fiction</Link>
+          <Link className="router-link" to="/scifi/">
+            <img src={scifi} alt="science fiction" />
+            <h3>Science Fiction</h3>
+          </Link>
         </div>
       </div>
       <div className="genre-bottom">
         <div className="genre-box genre-one">
-          <Link to="/horror/">Horror/Thriller</Link>
+          <Link className="router-link" to="/horror/">
+            <img src={creepydoll} alt="creepy doll pic" />
+            <h3>Horror/Thriller</h3>
+          </Link>
         </div>
-
         <div className="genre-box genre-two">
-          <Link to="/reality/">Reality/Documentary</Link>
+          <Link className="router-link" to="/reality/">
+            <img src={reality} alt="reality" />
+            <h3>Reality/Documentary</h3>
+          </Link>
         </div>
         <div className="genre-box genre-three">
-          <Link to="/other/">Other</Link>
+          <Link className="router-link" to="/other/">
+            <img src={other} alt="other options" />
+            <h3>Other</h3>
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,26 +1,43 @@
 import { Link, useLoaderData, Form } from "react-router-dom";
 import UpdateForm from "../components/UpdateForm";
+import React, { useState } from "react";
 
 function Show(props) {
+  const [visibleform, setVisiblefom] = useState(false);
+
+  const toggleForm = () => {
+    setVisiblefom(!visibleform);
+  };
+
   const aShow = useLoaderData();
   const id = aShow.url.split("/")[4];
   return (
     <div className="show-container">
       <div className="show-info">
-        <h2>{aShow.name}</h2>
-        <h3>({aShow.type})</h3>
-        <h4>
-          <b>Genre:</b> {aShow.genre}
-        </h4>
-        <h4>
-          <b>Recommended by:</b> {aShow.recby}
-        </h4>
+        <div className="show-image">
+          <h2>placeholder</h2>
+        </div>
+        <div className="show-deets">
+          <h2>{aShow.name}</h2>
+          <h3>({aShow.type})</h3>
+          <h4>
+            <b>Genre:</b> {aShow.genre}
+          </h4>
+          <h4>
+            <b>Recommended by:</b> {aShow.recby}
+          </h4>
+        </div>
       </div>
+
       <div className="show-options">
-        <UpdateForm />
         <Form action={`/delete/${id}/`} method="post">
           <button>Delete this.</button>
         </Form>
+
+        <button onClick={toggleForm}>
+          {visibleform ? "Nevermind." : "Edit this."}
+        </button>
+        {visibleform && <UpdateForm />}
       </div>
     </div>
   );
