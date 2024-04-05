80f64da6bd85b667f54adb094505570d887dc253
mackwick <mackenziewicker@gmail.com>
2024-04-05 15:14:24 -0400

    done with responsive styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,19 +14,19 @@ function Main(props) {
       <div className="genre-top">
         <div className="genre-box genre-one">
           <Link className="router-link" to="/drama/">
-            <img src={drama} alt="drama" />
+            <img src={drama} alt="drama" className="drama-pic" />
             <h3>Drama</h3>
           </Link>
         </div>
         <div className="genre-box genre-two">
           <Link className="router-link" to="/comedy/">
-            <img src={comedy} alt="comedy" />
+            <img src={comedy} alt="comedy" className="other-pic" />
             <h3>Comedy</h3>
           </Link>
         </div>
         <div className="genre-box genre-three">
           <Link className="router-link" to="/scifi/">
-            <img src={scifi} alt="science fiction" />
+            <img src={scifi} alt="science fiction" className="other-pic" />
             <h3>Science Fiction</h3>
           </Link>
         </div>
@@ -34,19 +34,19 @@ function Main(props) {
       <div className="genre-bottom">
         <div className="genre-box genre-one">
           <Link className="router-link" to="/horror/">
-            <img src={creepydoll} alt="creepy doll pic" />
+            <img src={creepydoll} alt="creepy doll pic" className="other-pic" />
             <h3>Horror/ Thriller</h3>
           </Link>
         </div>
         <div className="genre-box genre-two">
           <Link className="router-link" to="/reality/">
-            <img src={reality} alt="reality" />
+            <img src={reality} alt="reality" className="other-pic" />
             <h3>Reality/ Docs</h3>
           </Link>
         </div>
         <div className="genre-box genre-three">
           <Link className="router-link" to="/other/">
-            <img src={other} alt="other options" />
+            <img src={other} alt="other options" className="other-pic" />
             <h3>Other</h3>
           </Link>
         </div>
